b98e7cda0908159b0c0fb40bd1e247e32ff0d02e
Cheyoon Jung <jessietwo94@gmail.com>
2023-01-10 18:35:24 +0100

    update context and handle login

--- src/Context/authContext.tsx
+++ src/Context/authContext.tsx
@@ -1,6 +1,6 @@
 import { createContext, useContext, useEffect, useState } from "react";
 import { useAxios } from "../Components/Hook/useAxios";
-import { LoginResponse, Recipe, TokenResponse } from "./Types";
+import { LoginResponse } from "./Types";
 import { useNavigate } from "react-router-dom";
 
 const STORAGE_KEY = "begopa-user";
@@ -19,10 +19,20 @@ type ProviderProps = {
 const AuthContext = createContext({} as AuthContextType);
 
 export const AuthProvider = ({ children }: ProviderProps) => {
-  const { post } = useAxios();
   const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
   const [token, setToken] = useState<string>();
   const navigate = useNavigate();
+
+  useEffect(() => {
+    const storedData = localStorage.getItem(STORAGE_KEY);
+    if (!storedData) return;
+
+    const userData = JSON.parse(storedData);
+    const { token, expiresAt } = userData;
+
+    setToken(token);
+    setIsLoggedIn(true);
+  }, []);
 
   const handleLogin = (loginResponse: LoginResponse) => {
     const { token } = loginResponse;
--- src/Routes/SubmitRecipe.tsx
+++ src/Routes/SubmitRecipe.tsx
@@ -1,18 +1,43 @@
+import { Page } from "../Components/Page/Page.style";
+import { SubmitRecipeContainer } from "./SubmitRecipe.style";
+import { Formik } from "formik";
 interface Props {
   isEditMode?: Boolean;
 }
+const initialIngredient = { amount: "", unit: "", material: "" };
+
+const initialValues = {
+  name: "",
+  photoUrl: "",
+  cookingTime: "",
+  servings: "",
+  public: false,
+  instructions: "",
+  tags: [""],
+  ingredients: [{ ...initialIngredient }],
+};
 
 const SubmitRecipe = ({ isEditMode }: Props) => {
+  const submitForm = () => {
+    console.log("submit");
+  };
+
   return (
-    <>
-      <h1>home</h1>
-      <h1>home</h1>
-      <h1>home</h1>
-      <h1>home</h1>
-      <h1>home</h1>
-      <h1>home</h1>
-      <h1>home</h1>
-    </>
+    <Page>
+      <SubmitRecipeContainer>
+        <Formik initialValues={initialValues} onSubmit={submitForm}>
+          {({
+            values,
+            handleChange,
+            handleBlur,
+            handleSubmit,
+            errors,
+            touched,
+            isValid,
+          }) => <form onSubmit={handleSubmit}></form>}
+        </Formik>
+      </SubmitRecipeContainer>
+    </Page>
   );
 };
 
